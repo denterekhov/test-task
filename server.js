5585b6292e109bb30d7ee5cedb69f4f809f1ecb4
denterekhov <den.n.terekhov@gmail.com>
2019-03-24 18:11:07 +0200

    Updates

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const compression = require("compression");
-const path = require('path');
-const serveStatic = require('serve-static');
-const app = express();
-app.use(compression());
-app.use(serveStatic(__dirname));
-const port = process.env.PORT || 5000;
-app.listen(port);
